744cd023f6e430b619ac1744e66a8569f5a2ed0f
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-01-03 02:03:19 +0000

    Changed to use Pixel class, with console cursor positioning and colors

--- XmasTree.cs
+++ XmasTree.cs
@@ -1,17 +1,69 @@
-// Inspired by @LuigiArpino's Tweet, 25 Dec 2020, to @shanselman: https://twitter.com/luigiarpino/status/1342522409400860683
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using Crayon;
 using static System.Console;
 
-var n = int.TryParse(args.FirstOrDefault() ?? "15", out var nx) ? nx : 15;
-var rnd = new Random();
-for (var i = n; i > 1; i--) {
-  var t = "".PadLeft(i, ' ');
-  for (var j = 1; j < (n - i) * 2; j++)
-    t += rnd.Next(0, 10) > 7 ? '@' : '*';
-  WriteLine(t);
+{
+  Console.Clear();
+
+  var n = int.TryParse(args.FirstOrDefault() ?? "15", out var nx) ? nx : 15;
+  var xmasTree = BuildXmasTree(n);
+  foreach(var pixel in xmasTree)
+  {
+    pixel.Update();
+  }
+
+  SetCursorPosition(0, n + 4);
 }
-var b = "".PadLeft(n - 2, ' ') + "###";
-WriteLine(b);
-WriteLine(b);
-WriteLine(b);
+
+IReadOnlyList<Pixel> BuildXmasTree(int size)
+{
+  var pixels = new List<Pixel>();
+  var rnd = new Random();
+  for (var i = 1; i <= size; i++)
+  {
+    for (var j = 1; j < (size - i) * 2; j++)
+    {
+      var isLight = rnd.Next(0, 10) > 7;
+      pixels.Add(new Pixel(
+        i + j,
+        size - i,
+        isLight ? '@' : '*',
+        isLight ? (0xFF, 0xD7, 0x00) : (0x42, 0x69, 0x2F)));
+    }
+  }
+
+  for (var y = size; y < size + 3; y++)
+  {
+    for (var x = size - 1; x <= size + 1; x++)
+    {
+      pixels.Add(new Pixel(x, y, '#', (0x65, 0x43, 0x21)));
+    }
+  }
+
+  return pixels;
+}
+
+class Pixel
+{
+  public int X { get; init; }
+  public int Y { get; init; }
+  public string C { get; init; }
+
+  public (byte r, byte g, byte b) Color { get; init; }
+
+  public Pixel(int x, int y, char c, (byte, byte, byte) color)
+  {
+    X = x;
+    Y = y;
+    C = c.ToString();
+    Color = color;
+  }
+
+  public void Update()
+  {
+    SetCursorPosition(X, Y);
+    Write(Output.FromRgb(Color.r, Color.g, Color.b).Text(C));
+  }
+}
