2a343b1c8aa5b21323817f33cf0b8fd9b792e325
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2021-01-03 00:01:27 +0000

    Added "inspired by" comment, and using static System.Console

--- XmasTree.cs
+++ XmasTree.cs
@@ -1,4 +1,6 @@
+// Inspired by @LuigiArpino's Tweet, 25 Dec 2020, to @shanselman: https://twitter.com/luigiarpino/status/1342522409400860683
 using System;
+using static System.Console;
 
 var n = Convert.ToInt32(args[0]);
 var rnd = new Random();
@@ -6,9 +8,9 @@ for (var i = n; i > 1; i--) {
   var t = "".PadLeft(i, ' ');
   for (var j = 1; j < (n - i) * 2; j++)
     t += rnd.Next(0, 10) > 7 ? '@' : '*';
-  Console.WriteLine(t);
+  WriteLine(t);
 }
 var b = "".PadLeft(n - 2, ' ') + "###";
-Console.WriteLine(b);
-Console.WriteLine(b);
-Console.WriteLine(b);
+WriteLine(b);
+WriteLine(b);
+WriteLine(b);
